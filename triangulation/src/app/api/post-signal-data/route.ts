78b7cfa2e81cfe49dd4a9cee59bdfc8b304922d7
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-05 18:45:01 +0100

    push broken routing code

--- triangulation/src/app/api/post-signal-data/route.ts
+++ triangulation/src/app/api/post-signal-data/route.ts
@@ -3,16 +3,34 @@ import { NetworkTarget } from "@/lib/peripherals";
 export const networkObjects = new Map<string, NetworkTarget>()
 export async function POST(request: Request) {
     const body = await request.json();
+    
+    console.log(JSON.stringify(body));
 
-    if (!networkObjects.has(body.targetId)) {
-        networkObjects.set(body.targetId, new NetworkTarget(body.targetId));
+    if (
+        (!body.networkRssis) ||
+        (!body.id)
+    ) {
+        const response = new Response(null, {
+            status: 400, statusText: "Your network request sucks", headers: {
+                "Reply": "Bad-Job"
+            }
+        });
+        return response;
     }
 
-    const netTarget = networkObjects.get(body.targetId);
-    if (!netTarget) {
-        throw new Error("Network targets should exist!");
-    }
-    netTarget.addSignalData(body.peripheralId, body.signalData);
+    body.networkRssis.forEach((netw) => {
+        if (!networkObjects.has(netw["ssid"])) {
+            networkObjects.set(netw["ssid"], new NetworkTarget(netw["ssid"]));
+        }
+
+        const netTarget = networkObjects.get(netw.ssid);
+        if (!netTarget) {
+            throw new Error("Network targets should exist!");
+        }
+        netTarget.addSignalData(netw.ssid, netw.signalData);
+    });
+
+    
 
     const response = new Response(null, {
         status: 200, statusText: "OK", headers: {
@@ -21,3 +39,15 @@ export async function POST(request: Request) {
     });
     return response;
 }
+/*
+
+{
+networkRssis: [
+{
+ssid: string,
+rssi: -10,
+}
+],
+id: string
+}
+*/
