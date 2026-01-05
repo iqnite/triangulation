cffc4b91f0ffbed4469d1e74740633c7e6cd7ec1
iqnite <iqnitetechnologies@gmail.com>
2026-01-05 19:42:31 +0100

    Fix network

--- peripherals/Triangulation/src/main.cpp
+++ peripherals/Triangulation/src/main.cpp
@@ -27,6 +27,12 @@ void setup()
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.setAutoReconnect(true);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
   Serial.println("Connected. MAC: " + WiFi.macAddress());
   delay(100);
 }
