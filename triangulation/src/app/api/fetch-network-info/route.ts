7a977797086df0a28d8e84596c17ce898a821c07
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-06 16:59:25 +0100

    remove unused ready property

--- triangulation/src/app/api/fetch-network-info/route.ts
+++ triangulation/src/app/api/fetch-network-info/route.ts
@@ -3,7 +3,6 @@ import { readNetworks } from "@/lib/networkObjects";
 export type NetworkInfo = {
     ssid: string;
     pos: { x: number; y: number };
-    ready: boolean; //if the network has been located as a coordinate
 };
 
 export async function GET() {
@@ -12,7 +11,6 @@ export async function GET() {
             return {
                 ssid: net.address,
                 pos: { x: net.posAverage.x, y: net.posAverage.y },
-                ready: net.ready,
             };
         }) as NetworkInfo[],
         { status: 200 }
