52d06e2106ca0525f4f91d89f003d1e5f03797be
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-06 18:03:16 +0100

    fix radius scaling

--- triangulation/src/app/live/page.tsx
+++ triangulation/src/app/live/page.tsx
@@ -97,7 +97,7 @@ export default function Page() {
                         espId,
                         x: espPositions[espId][0],
                         y: espPositions[espId][1],
-                        r: dist
+                        r: dist / 60.67
                     })
                 })
             });
